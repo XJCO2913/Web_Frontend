999100df07e5d5a6ffe41f78ea8d0b3a2baac7d9
Loda174 <sc21y2c@leeds.ac.uk>
2024-04-15 18:31:08 +0800

    feat:complete the home page showslides

--- src/sections/home/home-carousel.jsx
+++ src/sections/home/home-carousel.jsx
@@ -1,4 +1,5 @@
 import { m } from 'framer-motion';
+// import { useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import Link from '@mui/material/Link';
@@ -8,6 +9,9 @@ import { alpha, useTheme } from '@mui/material/styles';
 
 import Image from 'src/components/image';
 import { varFade, MotionContainer } from 'src/components/animate';
+// import { useRouter } from 'src/routes/hooks';
+// import { paths } from 'src/routes/paths';
+
 // ----------------------------------------------------------------------
 
 export default function CarouselItem({ item, active }) {
@@ -15,13 +19,18 @@ export default function CarouselItem({ item, active }) {
 
   const { coverUrl, title, description } = item;
 
+  // const router = useRouter();
+
+  // const handleView = (id) => {
+  //   router.push(paths.home.tour.details(id));
+  // };
+
   const renderImg = (
     <Image
       alt={title}
       src={coverUrl}
-      overlay={`linear-gradient(to bottom, ${alpha(theme.palette.grey[900], 0)} 0%, ${
-        theme.palette.grey[900]
-      } 75%)`}
+      overlay={`linear-gradient(to bottom, ${alpha(theme.palette.grey[900], 0)} 0%, ${theme.palette.grey[900]
+        } 75%)`}
       sx={{
         width: 1,
         height: {
@@ -48,7 +57,7 @@ export default function CarouselItem({ item, active }) {
       >
         <m.div variants={varFade().inRight}>
           <Typography variant="overline" sx={{ color: 'primary.light' }}>
-            Featured App
+            Outdoor Activities
           </Typography>
         </m.div>
 
