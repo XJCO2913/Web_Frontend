f19c9491e2247c2a9288dde30ea1ed325d797844
Loda174 <sc21y2c@leeds.ac.uk>
2024-04-27 14:07:36 +0800

    fix: reset the timeout and restructure the map-view

--- src/sections/map-view/map.jsx
+++ src/sections/map-view/map.jsx
@@ -2,37 +2,19 @@ import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import AMapLoader from '@amap/amap-jsapi-loader';
 
-import { wgs2gcj, gcj2wgs } from './utils';
-import { useAuthContext } from '@/auth/hooks';
+import { useAuthContext } from 'src/auth/hooks';
+import UserIcon from './user-icon';
+import { wgs2gcj } from './utils';
+
+// ----------------------------------------------------------------------
 
 function Map({ isTracking }) {
     const mapRef = useRef(null);
-    const { user } = useAuthContext()
-
-    const iconHTML = `
-    <div style="
-    width: 30px; 
-    height: 30px; 
-    border-radius: 50%; 
-    background-color: white; 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 2px solid #00A76F;  /* 蓝色边框 */
-    overflow: hidden;  /* 确保图片超出部分不会显示 */"
-    >
-    <img src="${user?.avatarUrl}" alt="User Icon" style="
-        width: 30px; 
-        height: 30px;
-        border-radius: 50%;  
-        object-fit: cover;
-        pointer-events: none;"
-    />
-   </div>`;
+    const { user } = useAuthContext();
 
     useEffect(() => {
         AMapLoader.load({
-            key: "e65a2fad806f1efcbe741afff844c30b",
+            key: "your-api-key",
             version: "2.0",
             plugins: ["AMap.Geolocation", "AMap.Scale"],
         }).then((AMap) => {
@@ -43,7 +25,7 @@ function Map({ isTracking }) {
             });
             mapRef.current = map;
 
-            var geolocation = new AMap.Geolocation({
+            const geolocation = new AMap.Geolocation({
                 enableHighAccuracy: true,
                 timeout: 10000,
                 buttonPosition: 'RB',
@@ -52,41 +34,30 @@ function Map({ isTracking }) {
                 showCircle: false
             });
             map.addControl(geolocation);
-            geolocation.getCurrentPosition(function (status, result) {
-                if (status === 'complete') {
-                    // 使用从GPS设备获取到的坐标进行转换
-                    convertFrom([result.position.lng, result.position.lat], 'gps');
-                }
-            });
+            geolocation.getCurrentPosition((status, result) => handlePosition(status, result, map));
 
             const locationMarker = new AMap.Marker({
-                position: map.getCenter(), // 初始位置设为地图中心
+                position: map.getCenter(), 
                 map: map,
-                content: iconHTML
+                content: <UserIcon avatarUrl={user?.avatarUrl} />
             });
 
-            function convertFrom(lnglat) {
-                // 使用wgs2gcj进行坐标转换
-                const { lat, lng } = wgs2gcj(lnglat[1], lnglat[0]);
-
-                // 转换后的坐标设置为地图中心
-                const newCenter = new AMap.LngLat(lng, lat);
-                map("container", { zoom: 21 })
-                map.setCenter(newCenter);
-                locationMarker.setPosition(newCenter);
+            function handlePosition(status, result, map) {
+                if (status === 'complete') {
+                    const [lng, lat] = wgs2gcj(result.position.lat, result.position.lng);
+                    const newCenter = new AMap.LngLat(lng, lat);
+                    map.setCenter(newCenter);
+                    locationMarker.setPosition(newCenter);
+                }
             }
-
         }).catch(e => {
-            console.log(e);
+            console.error(e);
         });
 
         return () => {
-            if (mapRef.current) {
-                mapRef.current.destroy();
-                mapRef.current = null;
-            }
+            mapRef.current?.destroy();
         };
-    }, []);
+    }, [user]);
 
     return <div id="container" style={{ width: '100%', height: '100%' }}></div>;
 }
