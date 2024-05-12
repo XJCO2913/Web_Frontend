4e99eb384784fb515e047a03d225f407679c4f13
Loda174 <sc21y2c@leeds.ac.uk>
2024-05-12 10:05:47 +0800

    to:fix the download xml

--- src/layouts/common/notifications-popover/notification-item.jsx
+++ src/layouts/common/notifications-popover/notification-item.jsx
@@ -6,6 +6,7 @@ import Avatar from '@mui/material/Avatar';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemButton from '@mui/material/ListItemButton';
+import Button from '@mui/material/Button';
 
 import AMapPathDrawer from 'src/components/map'
 import { wgs2gcj } from 'src/utils/xml-shift'
@@ -13,6 +14,32 @@ import { wgs2gcj } from 'src/utils/xml-shift'
 // ----------------------------------------------------------------------
 
 export default function NotificationItem({ notification }) {
+
+  const convertArrayToXml = (route) => {
+    let xmlString = '<?xml version="1.0" encoding="UTF-8"?>\n<route>\n';
+    route.forEach((point, index) => {
+      xmlString += `  <point id="${index}">\n`;
+      xmlString += `    <latitude>${point[0]}</latitude>\n`;
+      xmlString += `    <longitude>${point[1]}</longitude>\n`;
+      xmlString += '  </point>\n';
+    });
+    xmlString += '</route>';
+    return xmlString;
+  };
+
+  const handleDownloadXml = (route) => {
+    const xmlData = convertArrayToXml(route);
+    const blob = new Blob([xmlData], { type: 'application/xml' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'route.xml';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url); // 清理创建的URL，防止内存泄漏
+  };
+
   const renderAvatar = (
     <ListItemAvatar>
       {notification.avatarUrl ? (
@@ -62,17 +89,24 @@ export default function NotificationItem({ notification }) {
 
   const routeAction = (
     notification.type === 'route' ?
-      <Stack spacing={1} direction="row" sx={{ mt: 1.5 }}>
-        <AMapPathDrawer
-          paths={[{
-            coords: notification?.route,
-            color: '#00A76F'
-          }]}
-          style={{ width: '100%', borderRadius: '8px' }}
-        />
-      </Stack> :
+      <>
+        <Stack spacing={1} direction="row" sx={{ mt: 1.5 }}>
+          <AMapPathDrawer
+            paths={[{
+              coords: notification?.route,
+              color: '#00A76F'
+            }]}
+            style={{ width: '100%', borderRadius: '8px' }}
+          />
+        </Stack>
+        <Button onClick={() => handleDownloadXml(notification?.route)} variant="contained" color="primary" sx={{mt: 1.5}}>
+          Download XML
+        </Button>
+      </>
+      :
       <></>
   );
+
 
   return (
     <ListItemButton
